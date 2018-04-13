fe88ce96c35a2652f06cce3f7e3bd0603010f82d
weilaa36 <37908253+weilaa36@users.noreply.github.com>
2018-04-13 12:06:16 -0500

    Add files via upload

---
+++ Activities/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using FYRAPP.Activities;
+
+namespace FYRAPP
+{
+    [Activity(Label = "FYRAPP", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        EditText txtUsername;
+        EditText txtPassword;
+        Button btnLogin;
+        Button btnRegister;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+
+            //Step 4: Making the buttons do something
+            btnLogin.Click += BtnLogin_Click;
+            btnRegister.Click += BtnRegister_Click;
+        }
+
+        private void BtnRegister_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(HomepageActivity));
+
+        }
+
+
+    }
+}
+
---
+++ Activities/RegisterActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FYRAPP.Activities
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        EditText txtCreateUsername;
+        EditText txtEmailAddress;
+        EditText txtCreatePassword;
+        EditText txtReenterPassword;
+        Button btnCreateAccount; 
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Register);
+
+            txtCreateUsername = FindViewById<EditText>(Resource.Id.txtCreateUsername);
+            txtEmailAddress = FindViewById<EditText>(Resource.Id.txtEmailAddress);
+            txtCreatePassword = FindViewById<EditText>(Resource.Id.txtCreatePassword);
+            txtReenterPassword = FindViewById<EditText>(Resource.Id.txtReenterPassword);
+            btnCreateAccount = FindViewById<Button>(Resource.Id.btnCreateAccount);
+
+           btnCreateAccount.Click += BtnCreateAccount_Click;
+        }
+
+        private void BtnCreateAccount_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(HomepageActivity));
+        }
+    }
+}
